ba1bfca187c34d309e8fd0996c9f9806ab1e41a0
LeoGillet <leo_gillet@protonmail.com>
2021-08-01 23:38:09 +0200

    Map count output stuck at 1

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ import sys
 def count_maps_from_path(pathname):
     if os.path.exists(os.path.join(pathname)):
         count = 0
+        gbxList_in_Dir = []
+        gbxList_in_Sub = []
         for root, dirs, files in os.walk(pathname):
-            count += len([f for f in files if f.endswith('.Gbx')])
-            count += len([f for f in files if f.endswith('.gbx')])
-            count += len([f for f in files if f.endswith('.GBX')])
-        print(f'Directory contains {count} GBX files.')
-        for root, dirs, files in os.walk(pathname):
-            return files
+            if len([f for f in files if f.endswith('.Gbx')]):
+                gbxList_in_Dir.append(files)
+                count += len([f for f in files if f.endswith('.Gbx')])
+            elif len([f for f in files if f.endswith('.gbx')]):
+                gbxList_in_Dir.append(files)
+                count += len([f for f in files if f.endswith('.Gbx')])
+            elif len([f for f in files if f.endswith('.GBX')]):
+                gbxList_in_Dir.append(files)
+                count += len([f for f in files if f.endswith('.Gbx')])
+        if count != 0:
+            print(f'Directory contains {count} GBX files.')
+            for maps in gbxList_in_Dir:
+                gbxList_in_Sub.append(maps)
+            print(gbxList_in_Sub)
+        else:
+            print('No GBX files found in directory.')
+
     else:
         print(f'Directory ({pathname}) doesn\'t exist, check syntax.')
 
