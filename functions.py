7b873f8a6c50374940ccdd6e06d3d9dbb999b321
LeoGillet <leo_gillet@protonmail.com>
2021-08-03 22:10:27 +0200

    Map count output stuck at 1

--- functions.py
+++ functions.py
@@ -7,19 +7,20 @@ def count_maps_from_path(pathname):
         gbxList_in_Sub = []
         for root, dirs, files in os.walk(pathname):
             if len([f for f in files if f.endswith('.Gbx')]):
-                gbxList_in_Dir.append(files)
-                count += len([f for f in files if f.endswith('.Gbx')])
+                for maps in files:
+                    gbxList_in_Dir.append(maps)
+                    count += 1
             elif len([f for f in files if f.endswith('.gbx')]):
-                gbxList_in_Dir.append(files)
-                count += len([f for f in files if f.endswith('.Gbx')])
+                for maps in files:
+                    gbxList_in_Dir.append(maps)
+                    count += 1
             elif len([f for f in files if f.endswith('.GBX')]):
-                gbxList_in_Dir.append(files)
-                count += len([f for f in files if f.endswith('.Gbx')])
+                for maps in files:
+                    gbxList_in_Dir.append(maps)
+                    count += 1
         if count != 0:
             print(f'Directory contains {count} GBX files.')
-            for maps in gbxList_in_Dir:
-                gbxList_in_Sub.append(maps)
-            print(gbxList_in_Sub)
+            print(gbxList_in_Dir)
         else:
             print('No GBX files found in directory.')
 
